91bd1bdd7525f5eca02d3d70819c49baf588a1b7
Austin Wolfe <wolfe_austin@yahoo.com>
2018-05-24 18:02:35 -0400

    Backend Day 2: Serializers

--- app/serializers/model_serializer.rb
+++ app/serializers/model_serializer.rb
@@ -1,3 +1,5 @@
 class ModelSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :category, :difficulty, :user_id, :description, :photo
+  has_many :steps
+  has_many :completed_models
 end
